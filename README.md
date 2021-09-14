976d7fdc7de543d89a76d667c66202351249cf66
ming.wang4 <ming.wang4@envision-digital.com>
2021-09-14 22:59:51 +0800

    init spring cloud nacose discovery

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # spring-cloud-hello-nacos-discovery
+
+###https://nacos.io/zh-cn/docs/quick-start-spring-cloud.html
+
+
+
+
---
+++ nacos-spring-cloud-consumer-example/src/test/java/com/example/nacosspringcloudconsumerexample/NacosSpringCloudConsumerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nacosspringcloudconsumerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosSpringCloudConsumerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
